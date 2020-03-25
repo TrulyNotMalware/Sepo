f632d85530b83826dc21af50a067b664f7606f88
YJH-W2N2 <yslle0@naver.com>
2020-03-25 17:08:22 +0900

    mouseEvent

--- Script1.js
+++ Script1.js
@@ -1,10 +1,3 @@
-function show_sub_nav(){
-    var sub_navs = document.getElementsByClassName("sub_nav_depth_1");
-    var Rep1 = 0;
-    for(Rep1;Rep1<sub_navs.length;Rep1++){
-        sub_navs[Rep1].addEventListener('mouseenter',function(e){
-            sub_navs[Rep1].style.display='block';
-            sub_navs;
-        });
-    }
+function test(){
+    alert("d");
 }
