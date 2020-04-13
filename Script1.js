d26b08eb252b6866c597422a2e4aa894ecd97882
YJH-W2N2 <yslle0@naver.com>
2020-04-13 15:38:03 +0900

    MouseEvent & Css update

--- Script1.js
+++ Script1.js
@@ -1,3 +1,20 @@
-function test(){
-    alert("d");
-}
+
+var Cursors =document.getElementsByClassName('Cursor');
+var subNavs =document.getElementsByClassName('sub_nav_depth_1');
+
+for(var Rep1=0;Rep1<subNavs.length;Rep1++){
+    (function(i){
+        var Cursor = Cursors.item(i);
+        var Nav = subNavs.item(i);
+        Cursor.addEventListener('mouseover',function(event){
+            Nav.style.display = "block";
+        });
+        Cursor.addEventListener('mouseout',function(event){
+            Nav.style.display = "none";
+        });
+        Nav.addEventListener('mouseover',function(event){ Nav.style.display="block";})
+        Nav.addEventListener('mouseout',function(event){
+            Nav.style.display = "none";
+        });
+    })(Rep1);
+}//Closure
