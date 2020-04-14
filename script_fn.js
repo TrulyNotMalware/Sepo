4834a914ffb49800005dde38d9031e696ceae62f
YJH-W2N2 <yslle0@naver.com>
2020-04-14 17:44:01 +0900

    sessions

--- script_fn.js
+++ script_fn.js
@@ -1,5 +1,5 @@
 function login_write(){
-    if(true){
+    if(sessionStorage.email!=user.ID){
         document.write('<li><a href=login.html>Login</a></li>');
     }else{
         document.write('<li><a href=login.html>Logout</a></li>');
