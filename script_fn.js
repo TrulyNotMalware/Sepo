b3939d64ee5ee944aced5d319da5c987cd007b44
Randomshot <sksghgus7175@gmail.com>
2020-04-16 14:13:51 +0900

    Delete script_fn.js

--- script_fn.js
+++
@@ -1,7 +0,0 @@
-function login_write(){
-    if(sessionStorage.email!=user.ID){
-        document.write('<li><a href=login.html>Login</a></li>');
-    }else{
-        document.write('<li><a href=login.html>Logout</a></li>');
-    }
-}
